7449318df0c9baaa4977a697d53124eec19ada9c
CJ <cjmendenhall24@hotmail.com>
2023-11-07 20:12:06 -0500

    typedefs and resolvers started

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const gql = require('apollo-server-express');
+
+const typeDefs = gql`
+enum Console {
+    NINTENDO
+    PLAYSTATION
+    XBOX
+}  
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    favConsole: [Console]
+}
+
+type Comment {
+    _id: ID
+    commentTitle: String
+    commentText: String
+    createdAt: String
+    username: String
+}
+
+type Query {
+    comments: [Comment]!
+    comment(commentId: ID!): Comment
+}
+`;
