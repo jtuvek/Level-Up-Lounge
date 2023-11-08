6515453d0ebc9f80169b749829659d2a23c3e399
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-07 20:12:10 -0500

    created user.js model

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true, // removes whitespace from both ends of a string
+        unique: true, // ensures that usernames are unique in the database
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // match: regex for email validation
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    favoriteConsole: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
