fbf3650ee060f7bbb9787f734982682517e6d7ac
Victor Nieves <c_cc_vic@yahoo.com>
2023-11-09 19:00:06 -0500

    created home component and styling

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+
+    return (  
+        <div className="home-container">
+            <div className="home">
+                <h1>Welcome to the Lounge!</h1>
+                <button className="join-btn">Join the Community</button>
+            </div>
+        </div>
+    );
+
+}
+
+export default Home;
