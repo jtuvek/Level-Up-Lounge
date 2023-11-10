32d37a36c7b3e0c8caac1aa6b6c5428a50115d58
Victor Nieves <c_cc_vic@yahoo.com>
2023-11-09 19:41:42 -0500

    updated code for Navbar and Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './index.css'
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/navbar.jsx'
+import Home from './components/Home.jsx'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-    
+      <Home />
     </div>
   )
 
