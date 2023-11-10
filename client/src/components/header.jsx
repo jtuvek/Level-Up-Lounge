1381bbce34360c110c615827e37f70e536af5d7e
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-09 19:03:56 -0500

    created header.jsx

---
+++ client/src/components/header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="header">
+        <h1></h1>
+        <a href="/" className="login-link">Login</a>
+        <a href="/" className="signup-link">SignUp</a>
+        </div>
+    );
+    }
+
+export default Header;
