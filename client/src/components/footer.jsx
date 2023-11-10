afe33ab0982edd793033669c31544bb4a316bdc1
CJ <cjmendenhall24@hotmail.com>
2023-11-09 19:07:52 -0500

    added footer component

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <nav className="footer">
+            <h6>Level Up Lounge</h6>
+            <div className="sidebar-links">
+                <a href="/" className="">Vic   </a>
+                <a href="/" className="">Josiah   </a>
+                <a href="/" className="">Cheik   </a>
+                <a href="/" className="">C.J.   </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Footer;
