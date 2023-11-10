1321805dc269d93d3e50354fb522570c084d8bfb
CJ <cjmendenhall24@hotmail.com>
2023-11-09 20:13:43 -0500

    header styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './index.css'
 import Navbar from './components/Navbar.jsx'
+import Footer from './components/footer.jsx'
+import Header from './components/header.jsx'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-    
-    </div>
+      <Header />
+      <Footer />
+       </div>
   )
 
 }
