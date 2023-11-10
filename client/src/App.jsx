afb62b2ec2f18c53e4cfd1010937dca8ebbd2d9e
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-09 19:59:11 -0500

    added imports for header and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import './index.css'
 import Navbar from './components/navbar.jsx'
 import Home from './components/Home.jsx'
+import Header from './components/header.jsx'; // Import the Header component
+import Footer from './components/footer.jsx'; // Import the Footer component
+
 
 function App() {
 
   return (
     <div className="App">
+      <Header /> {/* Render the Header component */}
       <Navbar />
       <Home />
+      <Footer /> {/* Render the Footer component */}
     </div>
   )
 
