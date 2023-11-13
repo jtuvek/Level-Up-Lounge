116568beff77849041a8c384c1f96d8a2e5bd5f9
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-13 18:20:27 -0500

    created pages.jsx files

---
+++ client/src/components/pages/Forums.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Forums = () => {
+  // Assuming you have a list of forums or discussion boards
+  const [forums, setForums] = useState([]);
+
+  useEffect(() => {
+    // Fetch forums data from your API or data source
+    // For simplicity, using a dummy data array here
+    const dummyForums = [
+      { id: 1, name: 'General Discussion' },
+      { id: 2, name: 'Game Reviews' },
+      { id: 3, name: 'Technical Support' },
+      // Add more forums as needed
+    ];
+
+    setForums(dummyForums);
+  }, []);
+
+  return (
+    <div className="forums-container">
+      <h2>Forums</h2>
+      <ul>
+        {forums.map((forum) => (
+          <li key={forum.id}>
+            <Link to={`/forums/${forum.id}`}>{forum.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Forums;
---
+++ client/src/components/pages/ProfilePage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfilePage = ({ user }) => {
+  return (
+    <div className="profile-container">
+      <div className="profile">
+        <h2>Profile Page</h2>
+        <div>
+          <strong>Username:</strong> {user.username}
+        </div>
+        <div>
+          <strong>Email:</strong> {user.email}
+        </div>
+        {/* Add more user details as needed */}
+      </div>
+    </div>
+  );
+};
+
+ProfilePage.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    // Add more prop types for other user details as needed
+  }).isRequired,
+};
+
+export default ProfilePage;
