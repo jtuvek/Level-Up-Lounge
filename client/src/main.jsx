3c51393a59a4bc0ec5503497cf8c2a7c9d9d6814
Victor Nieves <c_cc_vic@yahoo.com>
2023-11-13 19:39:43 -0500

    created Browser Router connection in mainjsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import App from './App.jsx'
+import Home from './components/Home.jsx'
+import ProfilePage from './components/pages/ProfilePage.jsx'
+import DiscussionBoards from './components/pages/DiscussionBoards.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "profile/:userId",
+        element: <ProfilePage />,
+      },
+      {
+        path: "forums",
+        element: <Forums />,
+      },
+      {
+        path: "discussion/:forumId",
+        element: <DiscussionBoards />,
+      },
+      
+    ],
+  },
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider />
 )
