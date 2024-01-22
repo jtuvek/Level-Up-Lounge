1cff3512f385e33118fbbfbeba1cec5dc8885297
Victor Nieves <c_cc_vic@yahoo.com>
2024-01-21 21:20:07 -0500

    added large image and text to homepage component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import kratos from "../assets/kratos.png"
 
 const Home = () => {
 
     return (  
-    
+    <>
         <div className="home">
             <h1>Welcome to Level Up Lounge!</h1>
             <div className='button-container'>
@@ -13,6 +14,17 @@ const Home = () => {
                 <button className="join-btn">Join the Community</button>
             </Link>
         </div>
+
+        {/* Why join Section */}
+        <div className="why-join-section">
+            <h2>Why join the Lounge?</h2>
+            <img
+                src={kratos}
+                alt="Kratos"
+                className='kratos-image'
+            />
+        </div>
+    </>
     
     );
 };
